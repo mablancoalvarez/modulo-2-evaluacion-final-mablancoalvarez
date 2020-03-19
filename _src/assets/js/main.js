6bbb5aa54ecbf5365e9f672c9e7395a730424ee2
mablancoalvarez <mablancoalvarez@gmail.com>
2020-03-19 21:09:35 +0100

    fix text from movie

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,7 +3,7 @@
 
 const inputSearch = document.querySelector('#input-search');
 const button = document.querySelector('#button-search');
-
+const img = document.querySelector('.imgElem');
 const ulElement = document.querySelector('#results');
 
 let movieList = null;
@@ -12,24 +12,28 @@ let movieList = null;
 function loadSeries() {
     ulElement.innerHTML = '';
     fetch(`http://api.tvmaze.com/search/shows?q=${inputSearch.value}`)
-    .then(response => response.json())
-    .then(data => {
-      movieList = data;
-    //  console.log(data);
-      renderMovies(movieList);
- });
+        .then(response => response.json())
+        .then(data => {
+            movieList = data;
+            //  console.log(data);
+            renderMovies(movieList);
+        });
 }
 // 2-show movies
 function renderMovies(movieArr) {
-  for (let movie of movieArr) {
-   ulElement.innerHTML += `<li id='${movie.show.id}' class='movies-list__item'><div class='movies-list__item-info'><img src=${movie.show.image.medium} alt=${movie.show.name}>
-   <span>${movie.show.name}</span></div></li>`;
+    for (let movie of movieArr) {
+
+        ulElement.innerHTML += `<li id='${movie.show.id}' class='movies-list__item'><div class='movies-list__item-info'><img class="imgElem" src=${movie.show.image.medium} alt=${movie.show.name}></div>
+    <span>${movie.show.name}</span></li>`;
         // console.log(movie);
 
+        // if (img !== null) {
+        //     img.src = movie.show.image.medium || movie.show.image.original;
+        // } else {
+        //     img.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+        // }
 
-
-        // `<li id=${movie.id}><div class='movies-list__item-info'><img src=${movie.image.medium} alt=${movie.name}></img><span>${movie.name}</span></div></li>`
-}
+    }
 }
 
 
