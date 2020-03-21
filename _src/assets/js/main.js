d4f6a4fae867754397e2cc671e00436c63a5edf4
mablancoalvarez <mablancoalvarez@gmail.com>
2020-03-21 14:22:21 +0100

    add button remove

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -19,9 +19,11 @@ function loadSeries() {
             movieList = data;
             //  console.log(data);
             renderMovies(movieList);
+            // Esto me pintara los datos
         });
 }
-// 2-show movies
+// 2-show movies me mostrara los resultados
+
 function renderMovies(movieArr) {
     for (let movie of movieArr) {
         if (movie.show.image !== null) {
@@ -34,7 +36,7 @@ function renderMovies(movieArr) {
         addListeners();
     }
 }
-
+// Añado los listeners a los li y seleccionaran la pelicula favorita
 function addListeners() {
     const liList = document.querySelectorAll('.movies-list__item');
     for (let li of liList) {
@@ -59,11 +61,25 @@ function readLocalStorage() {
 
 }
 
-//me quedo con el objeto
+// me quedo con el objeto
 function getMovieObject(id) {
     return movieList.find(movie => movie.show.id === parseInt(id))
 }
 
+//función que selecciona la película favorita
+//me quedo con el objeto , no con el id de la película seleccionada
+//y lo pusheo al array de favoritos
+//pinto los favoritos
+// function selectMovie(evt) {
+//     const selected = evt.currentTarget.id;
+//     const switchColor = evt.currentTarget;
+//     switchColor.setAttribute('class','switch')
+//     const object = getMovieObject(selected)
+//     selectedMovies.push(object.show);
+//     setLocalStorage();
+//     renderFavourites(selectedMovies);
+//     console.log(movieList)
+// }
 
 function selectMovie(evt) {
     const selected = evt.currentTarget.id;
@@ -71,8 +87,10 @@ function selectMovie(evt) {
     switchColor.setAttribute('class','switch')
     const object = getMovieObject(selected)
     selectedMovies.push(object.show);
+
     setLocalStorage();
     renderFavourites(selectedMovies);
+
     console.log(movieList)
 }
 
@@ -81,12 +99,31 @@ function renderFavourites(favArr) {
     console.log(selectedMovies)
     ulFav.innerHTML = '';
     for (let favourite of favArr) {
-            ulFav.innerHTML += `<li id='${favourite.id}' class='movies-list__item'><div class='movies-list__item-info'><img class="imgElem" src=${favourite.image.medium} alt=${favourite.name}></div>
-    <span>${favourite.name}</span></li>`;
-  
-        
+        // const object = getMovieObject(favArr)
+        // if (favArr = object.show.id) {
+
+            ulFav.innerHTML += `<li id='${favourite.id}' class='movies-list__item'><div class='movies-list__item-info'><i class='far fa-times-circle'></i><img class="imgElem" src=${favourite.image.medium} alt=${favourite.name}></div>
+            <span>${favourite.name}</span></li>`;
+        }
     }
-}
+
+
+//     function addFavouriteListeners (){
+//         const buttonList = document.querySelectorAll('button');
+//     for ( let button of buttonList ){
+//     button.addEventListener('click', removeMovie)
+    
+//     }
+//     }
+//     function removeMovie (evt){const elemId = evt.currentTarget.parentElement.id;
+//     // Esto se hace porque el currentt es el button y hay que llegar al padre que es el LI
+
+//     const elemIndex =  selectedMovies.indexOf(elemId);
+
+//     selectedMovies.splice(elemIndex,1);
+//     setLocalStorage();
+//     renderFavourites(selectedMovies);
+// }
 
 
 
